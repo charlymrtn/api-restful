53b687cbc6d3795031fa1484f2581ed2f6fa150e
Carlos Martín <cjmar87@gmail.com>
2018-10-18 14:15:24 -0500

    apunto de ejecutar las migraciones

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Database\Seeder;
 
+use App\User;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\Transaction;
+
+use DB;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +18,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+        User::truncate();
+        Category::truncate();
+        Product::truncate();
+        Transaction::truncate();
+        DB::table('category_product')->truncate():
+
+        $cant_users = 100;
+        $cant_categories = 20;
+        $cant_products = 500;
+        $cant_transactions = 500;
+
+        factory(User::class, $cant_users)->create();
+        factory(Category::class, $cant_categories)->create();
+
+        factory(Product::class, $cant_products)->create()->each(
+          function($product){
+            $categories = Category::all()->random(mt_rand(1,5))->pluck('uuid');
+
+            $product->categories()->attach($categories);
+          }
+        );
+        
+        factory(Transaction::class, $cant_transactions)->create();
     }
 }
